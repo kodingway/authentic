b9656d5fa19fec61f89334f13027834bbe97fdf2
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2013-03-05 14:23:17 +0100

    manage.py: move it to root of the project

--- manage.py
+++ manage.py
@@ -4,13 +4,16 @@ import os
 
 from django.core.management import execute_manager
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'vendor'))
+sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'authentic2', 'vendor'))
+
 try:
-    import settings # Assumed to be in the same directory.
+    import authentic2.settings as settings # Assumed to be in the same directory.
 except ImportError:
     import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
+
 
 if __name__ == "__main__":
     execute_manager(settings)
