9c299ebe956e343219b825fdd1676a8b2e928c16
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2013-03-08 11:38:07 +0100

    authentic2: expose User model in admin

--- authentic2/admin.py
+++ authentic2/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .nonce.models import Nonce
+from models import User
 
 class NonceModelAdmin(admin.ModelAdmin):
     list_display = ("value", "context", "not_on_or_after")
 
 admin.site.register(Nonce, NonceModelAdmin)
+admin.site.register(User, UserAdmin)
