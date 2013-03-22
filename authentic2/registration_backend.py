9a3e5cf8ddfecfd3dd5ed0faa34b011c9746acc5
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2013-03-22 17:56:42 +0100

    registration_backend: adapt simple registration backend for custom user model with required fields

---
+++ authentic2/registration_backend.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from registration.backends.simple import SimpleBackend as OldSimpleBackend
+from registration import signals
+
+try:
+    from django.contrib.auth import get_user_model
+except ImportError: # django < 1.5
+    from django.contrib.auth.models import User
+    get_user_model = lambda: User
+
+class SimpleBackend(OldSimpleBackend):
+    def register(self, request, **kwargs):
+        """
+        Create and immediately log in a new user.
+        """
+        create_kwargs = {
+                'username': kwargs['username'],
+                'email': kwargs['email'], 
+                'password': kwargs['password1']
+        }
+        for required_field in get_user_model().REQUIRED_FIELDS:
+            create_kwargs[required_field] = kwargs[required_field]
+        get_user_model().objects.create_user(**create_kwargs)
+
+        # authenticate() always has to be called before login(), and
+        # will return the user we just created.
+        new_user = authenticate(username=create_kwargs['username'], password=create_kwargs['password'])
+        login(request, new_user)
+        signals.user_registered.send(sender=self.__class__,
+                                     user=new_user,
+                                     request=request)
+        return new_user
