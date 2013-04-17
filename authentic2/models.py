98c85303fdfb99b453427559ef9609ca18499be1
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2013-04-17 18:08:41 +0200

    Revert "models: use our own PermissionMixin to set the related name on Permission and Group objects"
    This reverts commit effee8600ab5deb57064f0c6f8383e04f6dcac10.

--- authentic2/models.py
+++ authentic2/models.py
@@ -7,12 +7,12 @@ from django.core import validators
 from django.db import models
 from django.core.mail import send_mail
 from django.utils.translation import ugettext as _
-from django.contrib import auth
-from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
-        SiteProfileNotAvailable, Group, Permission, _user_get_all_permissions,
-        _user_has_perm, _user_has_module_perms)
+from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin,
+        BaseUserManager, SiteProfileNotAvailable)
 from django.contrib.auth import load_backend
 from django.utils.http import urlquote
+
+from idp.models import UserProfile
 
 
 class UserManager(BaseUserManager):
@@ -40,79 +40,6 @@ class UserManager(BaseUserManager):
         u.save(using=self._db)
         return u
 
-class PermissionsMixin(models.Model):
-    """
-    A mixin class that adds the fields and methods necessary to support
-    Django's Group and Permission model using the ModelBackend.
-    """
-    is_superuser = models.BooleanField(_('superuser status'), default=False,
-        help_text=_('Designates that this user has all permissions without '
-                    'explicitly assigning them.'))
-    groups = models.ManyToManyField(Group, verbose_name=_('groups'),
-        blank=True, help_text=_('The groups this user belongs to. A user will '
-                                'get all permissions granted to each of '
-                                'his/her group.'),
-        related_name='user_set')
-    user_permissions = models.ManyToManyField(Permission,
-        verbose_name=_('user permissions'), blank=True,
-        help_text='Specific permissions for this user.',
-        related_name='user_set')
-
-    class Meta:
-        abstract = True
-
-    def get_group_permissions(self, obj=None):
-        """
-        Returns a list of permission strings that this user has through his/her
-        groups. This method queries all available auth backends. If an object
-        is passed in, only permissions matching this object are returned.
-        """
-        permissions = set()
-        for backend in auth.get_backends():
-            if hasattr(backend, "get_group_permissions"):
-                permissions.update(backend.get_group_permissions(self, obj))
-        return permissions
-
-    def get_all_permissions(self, obj=None):
-        return _user_get_all_permissions(self, obj)
-
-    def has_perm(self, perm, obj=None):
-        """
-        Returns True if the user has the specified permission. This method
-        queries all available auth backends, but returns immediately if any
-        backend returns True. Thus, a user who has permission from a single
-        auth backend is assumed to have permission in general. If an object is
-        provided, permissions for this specific object are checked.
-        """
-
-        # Active superusers have all permissions.
-        if self.is_active and self.is_superuser:
-            return True
-
-        # Otherwise we need to check the backends.
-        return _user_has_perm(self, perm, obj)
-
-    def has_perms(self, perm_list, obj=None):
-        """
-        Returns True if the user has each of the specified permissions. If
-        object is passed, it checks if the user has all required perms for this
-        object.
-        """
-        for perm in perm_list:
-            if not self.has_perm(perm, obj):
-                return False
-        return True
-
-    def has_module_perms(self, app_label):
-        """
-        Returns True if the user has any permissions in the given app label.
-        Uses pretty much the same logic as has_perm, above.
-        """
-        # Active superusers have all permissions.
-        if self.is_active and self.is_superuser:
-            return True
-
-        return _user_has_module_perms(self, app_label)
 
 class AbstractUser(AbstractBaseUser, PermissionsMixin):
     """
@@ -121,7 +48,7 @@ class AbstractUser(AbstractBaseUser, PermissionsMixin):
 
     Username, password and email are required. Other fields are optional.
     """
-    username = models.CharField(_('username'), max_length=128, unique=True,
+    username = models.CharField(_('username'), max_length=30, unique=True,
         help_text=_('Required. 30 characters or fewer. Letters, numbers and '
                     '@/./+/-/_ characters'),
         validators=[
