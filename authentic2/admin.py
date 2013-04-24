9133738a00d64539dfa11bef7551e35a91420e5b
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2013-04-24 09:55:13 +0200

    admin: only expose the user model if it is used

--- authentic2/admin.py
+++ authentic2/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.conf import settings
 
 from .nonce.models import Nonce
 from models import User
@@ -8,4 +9,5 @@ class NonceModelAdmin(admin.ModelAdmin):
     list_display = ("value", "context", "not_on_or_after")
 
 admin.site.register(Nonce, NonceModelAdmin)
-admin.site.register(User, UserAdmin)
+if settings.AUTH_USER_MODEL == 'authentic2.User':
+    admin.site.register(User, UserAdmin)
