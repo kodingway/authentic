0e212ca872a73c5c692b5552ec81681fa38be9c0
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2013-05-13 10:15:22 +0200

    remove check-sdist.sh, comparable functionnality is in the python-entrouvert package

--- check-sdist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-VERSION=`cat authentic2/__init__.py | grep VERSION | sed 's/VERSION = "\(.*\)"/\1/'`
-
-
-rm -f dist/authentic2-$VERSION.tar.gz && \
-python setup.py sdist && \
-diff <(git ls-files|sort) <(tar tf dist/authentic2-$VERSION.tar.gz|sort|grep -v '/$'|sed "s#.*-$VERSION/##")
