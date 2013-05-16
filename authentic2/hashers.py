5d3bbf8e4629f64cbdc28eeb8554d521fa6cca41
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2013-05-16 14:15:41 +0200

    hashers: add SHA256PasswordHasher

--- authentic2/hashers.py
+++ authentic2/hashers.py
@@ -82,3 +82,35 @@ class Drupal7PasswordHasher(hashers.BasePasswordHasher):
             (_('salt'), hashers.mask_hash(salt)),
             (_('hash'), hashers.mask_hash(hash)),
         ])
+
+class CommonPasswordHasher(hashers.BasePasswordHasher):
+    """
+    The Salted MD5 password hashing algorithm (not recommended)
+    """
+    algorithm = None
+    digest = None
+
+    def encode(self, password, salt):
+        assert password
+        assert salt and '$' not in salt
+        hash = self.digest(force_bytes(salt + password)).hexdigest()
+        return "%s$%s$%s" % (self.algorithm, salt, hash)
+
+    def verify(self, password, encoded):
+        algorithm, salt, hash = encoded.split('$', 2)
+        assert algorithm == self.algorithm
+        encoded_2 = self.encode(password, salt)
+        return constant_time_compare(encoded, encoded_2)
+
+    def safe_summary(self, encoded):
+        algorithm, salt, hash = encoded.split('$', 2)
+        assert algorithm == self.algorithm
+        return SortedDict([
+            (_('algorithm'), algorithm),
+            (_('salt'), hashers.mask_hash(salt, show=2)),
+            (_('hash'), hashers.mask_hash(hash)),
+        ])
+
+class SHA256PasswordHasher(CommonPasswordHasher):
+    algorithm = 'sha256'
+    digest = hashlib.sha256
