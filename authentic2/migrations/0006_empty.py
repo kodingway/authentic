46166ce62f729a14e12cf776cf7abb7b5ef09601
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2013-06-20 09:32:01 +0200

    models: create new app around model authentic2.User
    Authentic should continue to work with the default user model.

--- authentic2/migrations/0006_empty.py
+++ authentic2/migrations/0006_empty.py
@@ -1,20 +1,17 @@
 # -*- coding: utf-8 -*-
-import datetime
 from south.db import db
 from south.v2 import SchemaMigration
-from django.db import models
 
 
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        db.rename_table('auth_user', 'authentic2_user')
+        pass
 
     def backwards(self, orm):
-        db.rename_table('authentc2_user', 'auth_user')
+        pass
 
     models = {
-        
     }
 
     complete_apps = ['authentic2']
