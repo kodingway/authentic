a19448262224d1881b4fa6aa042e02d10741ffa0
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2013-06-20 09:36:23 +0200

    auth2_auth: clean migrations

--- authentic2/auth2_auth/migrations/0002_auto__chg_field_authenticationevent_nonce.py
+++ authentic2/auth2_auth/migrations/0002_auto__chg_field_authenticationevent_nonce.py
@@ -1,19 +1,15 @@
 # encoding: utf-8
-import datetime
 from south.db import db
 from south.v2 import SchemaMigration
-from django.db import models
 
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
         # Changing field 'AuthenticationEvent.nonce'
         db.alter_column('auth2_auth_authenticationevent', 'nonce', self.gf('django.db.models.fields.CharField')(max_length=255))
 
 
     def backwards(self, orm):
-        
         # Changing field 'AuthenticationEvent.nonce'
         db.alter_column('auth2_auth_authenticationevent', 'nonce', self.gf('django.db.models.fields.CharField')(max_length=20))
 
