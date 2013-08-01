kodingway/authentic

commit 7ab2405a888c383e515ba25d3a8142059caacc0a

idp_cas: when the service url already contains a ? character, use a & instead of ? to add new parameters
Sponsored by http://www.ill.eu/ (Institut Laue-Langevin)

authentic2/idp/idp_cas/views.py
@@ -230,8 +230,10 @@ renew:%s and gateway:%s' % (service, renew, gateway))
             return self.return_ticket(request, st)
 
     def return_ticket(self, request, st):
-        return HttpResponseRedirect('%s?ticket=%s' % (st.service,
-            st.ticket_id))
+        if '?' in st.service:
+               return HttpResponseRedirect('%s&ticket=%s' % (st.service,st.ticket_id))
+       else:
+               return HttpResponseRedirect('%s?ticket=%s' % (st.service,st.ticket_id))
 
     def validate(self, request):
         if request.method != 'GET':

commit 2b09e18a78171d7caa24797d5843b84b53e516ca

idp_cas/views: fix indentation error introduced in commit 7ab2405a

authentic2/idp/idp_cas/views.py
@@ -231,9 +231,9 @@ renew:%s and gateway:%s' % (service, renew, gateway))
 
     def return_ticket(self, request, st):
         if '?' in st.service:
-               return HttpResponseRedirect('%s&ticket=%s' % (st.service,st.ticket_id))
-       else:
-               return HttpResponseRedirect('%s?ticket=%s' % (st.service,st.ticket_id))
+            return HttpResponseRedirect('%s&ticket=%s' % (st.service,st.ticket_id))
+        else:
+            return HttpResponseRedirect('%s?ticket=%s' % (st.service,st.ticket_id))
 
     def validate(self, request):
         if request.method != 'GET':