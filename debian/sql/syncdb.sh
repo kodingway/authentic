ba31493d550ebf3c629299f8c6d3d16d0749e743
Jérôme Schneider <jschneider@entrouvert.com>
2013-11-19 15:48:33 +0100

    fix db installation

--- debian/sql/syncdb.sh
+++ debian/sql/syncdb.sh
@@ -2,6 +2,6 @@
 
 . /etc/authentic2/authentic.conf
 
-python /usr/lib/authentic2/manage.py syncdb --noinput
+python /usr/lib/authentic2/manage.py syncdb --all --noinput
 python /usr/lib/authentic2/manage.py migrate --fake --noinput
 
