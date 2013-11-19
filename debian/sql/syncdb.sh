8906f72451a5620a5feafa1500fe169891242cc7
Jérôme Schneider <jschneider@entrouvert.com>
2013-11-19 15:45:34 +0100

    Move config files into folders

--- debian/sql/syncdb.sh
+++ debian/sql/syncdb.sh
@@ -3,5 +3,5 @@
 . /etc/authentic2/authentic.conf
 
 python /usr/lib/authentic2/manage.py syncdb --noinput
-python /usr/lib/authentic2/manage.py migrate
+python /usr/lib/authentic2/manage.py migrate --fake --noinput
 
