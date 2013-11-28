08b54968da9ee0b615ec6abb8a5391da2c58ada2
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2013-11-28 14:50:55 +0100

    idp/utils: add a get_username method to adapt to new custom user models

--- authentic2/idp/utils.py
+++ authentic2/idp/utils.py
@@ -5,3 +5,10 @@ class Service(object):
 
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
+
+def get_username(user):
+    '''Retrieve the username from a user model'''
+    if hasattr(user, 'USERNAME_FIELD'):
+        return getattr(user, user.USERNAME_FIELD)
+    else:
+        return user.username
