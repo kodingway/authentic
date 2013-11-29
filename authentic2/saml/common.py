881e02b162fa0d34c6c635be3f3351a90062c99b
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2013-11-29 16:31:31 +0100

    saml/common: in load_federation() user is mandatory, so add an assertion

--- authentic2/saml/common.py
+++ authentic2/saml/common.py
@@ -246,6 +246,7 @@ def load_federation(request, entity_id, login, user = None):
     '''Load an identity dump from the database'''
     if not user:
         user = request.user
+    assert user is not None
     logger.debug('load_federation: user is %r' % user)
     identity_dump = federations_to_identity_dump(entity_id,
             LibertyFederation.objects.filter(user=user))
