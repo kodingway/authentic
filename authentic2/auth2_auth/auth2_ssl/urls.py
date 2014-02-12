21b4e110328094691d1960e7e2ddc4b258df7e40
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-02-12 14:14:02 +0100

    auth2_ssl: refactoring [module reorg]
     - move all views in the views module
     - fix use of url() in the urls module

--- authentic2/auth2_auth/auth2_ssl/urls.py
+++ authentic2/auth2_auth/auth2_ssl/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('authentic2.auth2_auth.auth2_ssl.login_ssl',
+urlpatterns = patterns('authentic2.auth2_auth.auth2_ssl.views',
     url(r'^$', 'handle_request', name='user_signin_ssl'),
-    url(r'^post_account_linking/$', name='post_account_linking'),
-    url(r'^delete_certificate/$', name='delete_certificate'),
-    url(r'^register/$', 'views.register', name='sslauth_register'),
+    url(r'^post_account_linking/$', 'post_account_linking',
+        name='post_account_linking'),
+    url(r'^delete_certificate/$', 'delete_certificate',
+        name='delete_certificate'),
+    url(r'^register/$', 'register', name='sslauth_register'),
     url(r'^error_ssl/$', 'error_ssl', name='error_ssl'),
 )
