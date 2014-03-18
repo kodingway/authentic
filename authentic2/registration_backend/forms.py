34ad76d6d6c17433e6f3dabee5ef1546dbac8a37
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-03-18 16:21:38 +0100

    registration_backend/forms.py: validate that passwords match on registration

--- authentic2/registration_backend/forms.py
+++ authentic2/registration_backend/forms.py
@@ -50,3 +50,15 @@ class RegistrationForm(forms.Form):
                 raise forms.ValidationError(_('This email address is already in '
                     'use. Please supply a different email address.'))
         return self.cleaned_data['email']
+
+    def clean(self):
+        """
+        Verifiy that the values entered into the two password fields
+        match. Note that an error here will end up in
+        ``non_field_errors()`` because it doesn't apply to a single
+        field.
+        """
+        if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
+            if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+                raise forms.ValidationError(_("The two password fields didn't match."))
+        return self.cleaned_data
