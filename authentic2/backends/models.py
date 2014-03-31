f1065055b37d096baa6e0daebf80b9dacb73d44e
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-03-31 19:37:21 +0200

    backends: in the model backend try to lookup user also by appending known realms

--- authentic2/backends/models.py
+++ authentic2/backends/models.py
@@ -10,16 +10,30 @@ class ModelBackend(ModelBackend):
     Authenticates against settings.AUTH_USER_MODEL.
     """
 
-    def authenticate(self, username=None, password=None, **kwargs):
+    def get_query(self, username):
         UserModel = get_user_model()
-        if username is None:
-            username = kwargs.get(UserModel.USERNAME_FIELD)
-        query = models.Q(**{UserModel.USERNAME_FIELD: username})
+        username_field = UserModel.USERNAME_FIELD
+        query = models.Q(**{username_field: username})
         try:
             if app_settings.ACCEPT_EMAIL_AUTHENTICATION and UserModel._meta.get_field('email'):
                 query |= models.Q(**{'email': username})
         except models.FieldDoesNotExist:
             pass
+        if '@' not in username:
+            if app_settings.A2_REGISTRATION_REALM:
+                u = u'{0}@{1}'.format(username, app_settings.A2_REGISTRATION_REALM)
+                query |= models.Q(**{username_field: u})
+            if app_settings.A2_REALMS:
+                for realm in app_settings.A2_REALMS:
+                    u = u'{0}@{1}'.format(username, realm)
+                    query |= models.Q(**{username_field: u})
+        return query
+
+    def authenticate(self, username=None, password=None, **kwargs):
+        UserModel = get_user_model()
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
+        query = self.get_query(username)
         for user in UserModel._default_manager.filter(query):
             if user.check_password(password):
                 return user
