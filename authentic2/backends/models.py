ab20beec0765030deafaad00a0975671e1e49e99
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-04-01 10:57:14 +0200

    backends/models: if the login form pass a realm use it

--- authentic2/backends/models.py
+++ authentic2/backends/models.py
@@ -1,39 +1,50 @@
 from __future__ import unicode_literals
+
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 
 from .. import app_settings
 
+def upn(username, realm):
+    '''Build an UPN from a username and a realm'''
+    return u'{0}@{1}'.format(username, realm)
+
 class ModelBackend(ModelBackend):
     """
     Authenticates against settings.AUTH_USER_MODEL.
     """
 
-    def get_query(self, username):
+    def get_query(self, username, realm):
         UserModel = get_user_model()
         username_field = UserModel.USERNAME_FIELD
-        query = models.Q(**{username_field: username})
+        queries = []
         try:
-            if app_settings.ACCEPT_EMAIL_AUTHENTICATION and UserModel._meta.get_field('email'):
-                query |= models.Q(**{'email': username})
+            if app_settings.ACCEPT_EMAIL_AUTHENTICATION \
+                    and UserModel._meta.get_field('email'):
+                queries.append(models.Q(**{'email': username}))
         except models.FieldDoesNotExist:
             pass
-        if '@' not in username:
-            if app_settings.A2_REGISTRATION_REALM:
-                u = u'{0}@{1}'.format(username, app_settings.A2_REGISTRATION_REALM)
-                query |= models.Q(**{username_field: u})
-            if app_settings.A2_REALMS:
-                for realm in app_settings.A2_REALMS:
-                    u = u'{0}@{1}'.format(username, realm)
-                    query |= models.Q(**{username_field: u})
-        return query
 
-    def authenticate(self, username=None, password=None, **kwargs):
+        if realm is None:
+            queries.append(models.Q(**{username_field: username}))
+            if '@' not in username:
+                if app_settings.A2_REGISTRATION_REALM:
+                    queries.append(models.Q(**{username_field:
+                        upn(username, app_settings.A2_REGISTRATION_REALM)}))
+                if app_settings.A2_REALMS:
+                    for realm in app_settings.A2_REALMS:
+                        queries.append(models.Q(
+                                **{username_field: upn(username, realm)}))
+        else:
+            queries.append(models.Q(**{username_field: upn(username, realm)}))
+        return reduce(models.Q.__or__, queries)
+
+    def authenticate(self, username=None, password=None, realm=None, **kwargs):
         UserModel = get_user_model()
         if username is None:
             username = kwargs.get(UserModel.USERNAME_FIELD)
-        query = self.get_query(username)
+        query = self.get_query(username, realm)
         for user in UserModel._default_manager.filter(query):
             if user.check_password(password):
                 return user
