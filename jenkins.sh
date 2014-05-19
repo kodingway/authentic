2be7bdf8946d4e7477da7aea1a4fc77bac45170f
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-05-19 11:51:36 +0200

    jenkins.sh: restrict pylint to version 1.1.0 which raised less warnings

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pip install --upgrade pip
-pip install --upgrade pylint
+pip install --upgrade pylint==1.1.0
 pip install --upgrade -v -r requirements.txt
 pip install --upgrade django-authopenid
 export SECRET_KEY='coin'
