4930227c8af4a4cf27f773ef9a7363e72eee4fa5
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-07-18 15:25:57 +0200

    compat: add work-around for Django < 1.8 and commit_on_success

--- authentic2/compat.py
+++ authentic2/compat.py
@@ -6,6 +6,12 @@ except ImportError:
     from django.contrib.auth.models import User
     get_user_model = lambda: User
 
+try:
+    from django.db.transaction import atomic
+    commit_on_success = atomic
+except ImportError:
+    from django.db.transaction import commit_on_success
+
 from . import app_settings, utils
 
 user_model_label = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
