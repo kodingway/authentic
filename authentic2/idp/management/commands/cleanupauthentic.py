kodingway/authentic

commit 432f7945f68801e04fbf78df6fbb6dd056364a8b

Log tracebacks in cleaning commands (fixes #3458)

authentic2/idp/management/commands/cleanupauthentic.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.core.management.base import NoArgsCommand
 from django.db import models
 
@@ -5,13 +7,19 @@ class Command(NoArgsCommand):
     help = 'Clean expired models of authentic2.'
 
     def handle_noargs(self, **options):
+        log = logging.getLogger(__name__)
+
         for app in models.get_apps():
             for model in models.get_models(app):
                 # only models from authentic2
                 if not model.__module__.startswith('authentic2'):
                     continue
-                manager = getattr(model, 'objects', None)
                 try:
-                    manager.cleanup()
-                except (AttributeError, TypeError):
-                    pass
+                    self.cleanup_model(model)
+                except:
+                    log.exception('cleanup of model %s failed', model)
+
+    def cleanup_model(model):
+        manager = getattr(model, 'objects', None)
+        if hasattr(manager, 'cleanup'):
+            manager.cleanup()

authentic2/management/commands/clean-unused-accounts.py
@@ -41,6 +41,13 @@ class Command(BaseCommand):
     )
 
     def handle(self, *args, **options):
+        log = logging.getLogger(__name__)
+        try:
+            self.clean_unused_acccounts(*args, **options)
+        except:
+            log.exception('failure while cleaning unused accounts')
+
+    def clean_unused_acccounts(self, *args, **options):
         if len(args) < 1:
             raise CommandError('missing clean_threshold')
         if options['period'] < 1:

commit 0283a7c31ad77c175113cbc2474b148daafeb1db

Fix typo in cleanupauthentic command (fixes #3458)
Bug introduced in 432f7945.

authentic2/idp/management/commands/cleanupauthentic.py
@@ -19,7 +19,7 @@ class Command(NoArgsCommand):
                 except:
                     log.exception('cleanup of model %s failed', model)
 
-    def cleanup_model(model):
+    def cleanup_model(self, model):
         manager = getattr(model, 'objects', None)
         if hasattr(manager, 'cleanup'):
             manager.cleanup()