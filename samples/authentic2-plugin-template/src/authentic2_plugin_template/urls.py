5089911c7f1d9e2634c79df54d642a027c5c358f
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-09-10 12:30:01 +0200

    Use new setting_enabled and required decorators in plugin sample

--- samples/authentic2-plugin-template/src/authentic2_plugin_template/urls.py
+++ samples/authentic2-plugin-template/src/authentic2_plugin_template/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import patterns, url
 
+from authentic2.decorators import setting_enabled, required
+
+from . import app_settings
 from .views import index
 
-urlpatterns = patterns('',
-        url('^authentic2_plugin_template/$', index,
-            name='authentic2-plugin-template-index'),
+urlpatterns = required(
+        setting_enabled('ENABLE', settings=app_settings),
+        patterns('',
+            url('^authentic2_plugin_template/$', index,
+                name='authentic2-plugin-template-index'),
+        )
 )
