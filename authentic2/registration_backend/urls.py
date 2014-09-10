0fd7812e5f971a50720af3be0bf13d7ebe52b7ec
Serghei MIHAI <smihai@entrouvert.com>
2014-09-11 01:45:47 +0200

    urls not involved in registration process removed from registration backend

--- authentic2/registration_backend/urls.py
+++ authentic2/registration_backend/urls.py
@@ -1,18 +1,10 @@
 from django.conf.urls import patterns
 from django.conf.urls import url
 from django.utils.importlib import import_module
-from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
+from django.contrib.auth.decorators import login_required
 
-from authentic2.utils import get_form_class
-from .. import app_settings
-from .views import RegistrationView, ActivationView
-
-SET_PASSWORD_FORM_CLASS = get_form_class(
-        app_settings.A2_REGISTRATION_SET_PASSWORD_FORM_CLASS)
-CHANGE_PASSWORD_FORM_CLASS = get_form_class(
-        app_settings.A2_REGISTRATION_CHANGE_PASSWORD_FORM_CLASS)
-
+from .views import RegistrationView, ActivationView, DeleteView
 
 urlpatterns = patterns('authentic2.registration_backend.views',
     url(r'^activate/complete/$',
@@ -34,27 +26,7 @@ urlpatterns = patterns('authentic2.registration_backend.views',
     url(r'^register/closed/$',
         TemplateView.as_view(template_name='registration/registration_closed.html'),
         name='registration_disallowed'),
-    url(r'^password/change/$',
-        auth_views.password_change,
-        {'password_change_form': CHANGE_PASSWORD_FORM_CLASS},
-        name='auth_password_change'),
-    url(r'^password/change/done/$',
-        auth_views.password_change_done,
-        name='auth_password_change_done'),
-    url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        auth_views.password_reset_confirm,
-        {'set_password_form': SET_PASSWORD_FORM_CLASS},
-        name='auth_password_reset_confirm'),
     url(r'^delete/$',
-        'delete',
+        login_required(DeleteView.as_view()),
         name='delete_account'),
-    url(r'^password/reset/$',
-        auth_views.password_reset,
-        name='auth_password_reset'),
-    url(r'^password/reset/complete/$',
-        auth_views.password_reset_complete,
-        name='auth_password_reset_complete'),
-    url(r'^password/reset/done/$',
-        auth_views.password_reset_done,
-        name='auth_password_reset_done'),
 )
