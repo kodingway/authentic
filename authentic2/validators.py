288262f03f978bd67c441ff936ae1d91f13a637b
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-10-17 14:52:00 +0200

    Do not reuse Django EmailValidator in our own validator
    We already do all their checks and more.
    
    refs #5743

--- authentic2/validators.py
+++ authentic2/validators.py
@@ -5,7 +5,6 @@ import smtplib
 
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import force_text
-from django.core.validators import EmailValidator, email_re
 from django.core.exceptions import ValidationError
 
 import socket
@@ -15,10 +14,9 @@ import dns.exception
 from . import app_settings
 
 # copied from http://www.djangotips.com/real-email-validation
-class EmailValidator(EmailValidator):
-    def __init__(self, *args, **kwargs):
-        self.rcpt_check = kwargs.pop('rcpt_check', True)
-        super(EmailValidator, self).__init__(*args, **kwargs)
+class EmailValidator(object):
+    def __init__(self, rcpt_check=False):
+        self.rcpt_check = rcpt_check
 
     def check_mxs(self, domain):
         try:
@@ -35,15 +33,14 @@ class EmailValidator(EmailValidator):
 
 
     def __call__(self, value):
-        super(EmailValidator, self).__call__(value)
         try:
             hostname = value.split('@')[-1]
         except KeyError:
-            raise ValidationError(_('Enter a valid email address.'))
+            raise ValidationError(_('Enter a valid email address.'), code='invalid-email')
 
         mxs = self.check_mxs(hostname)
         if not mxs:
-            raise ValidationError(_('Email domain is invalid'))
+            raise ValidationError(_('Email domain is invalid'), code='invalid-domain')
 
         if not self.rcpt_check or not app_settings.A2_VALIDATE_EMAIL:
             return
@@ -59,23 +56,20 @@ class EmailValidator(EmailValidator):
                     smtp.mail('')
                     status = smtp.rcpt(value)
                     if status[0] % 100 == 5:
-                        raise ValidationError(_('Invalid email address.'))
+                        raise ValidationError(_('Invalid email address.'), code='rcpt-check-failed')
                     break
                 except smtplib.SMTPServerDisconnected:
                     break
                 except smtplib.SMTPConnectError:
                     continue
+        # Should not happen !
         except dns.resolver.NXDOMAIN:
             raise ValidationError(_('Nonexistent domain.'))
         except dns.resolver.NoAnswer:
             raise ValidationError(_('Nonexistent email address.'))
 
 
-validate_email = EmailValidator(
-    email_re,
-    _('Enter a valid email address.'),
-    'invalid'
-)
+validate_email = EmailValidator()
 
 def validate_password(password):
     password_set = set(password)
