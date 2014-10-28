54d03aa54a36822d7c297aa5c118727d7d88d4c0
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-10-28 17:07:18 +0100

    Convert urls.py in OpenID IdP for direct import of views (refs #5960)

--- authentic2/idp/idp_openid/urls.py
+++ authentic2/idp/idp_openid/urls.py
@@ -1,16 +1,26 @@
 # vim: set ts=4 sw=4 : */
 
 from django.conf.urls import patterns, url
-from views import TrustedRootDelete
+from . import views
 
 urlpatterns = patterns('authentic2.idp.idp_openid.views',
-    url(r'^$', 'openid_server', name='openid-provider-root'),
-    url(r'^trustedroot/(?P<pk>\d+)/delete/$', TrustedRootDelete.as_view(),
+    url(r'^$',
+        views.openid_server,
+        name='openid-provider-root'),
+    url(r'^trustedroot/(?P<pk>\d+)/delete/$',
+        views.openid_trustedroot_delete,
         name='trustedroot_delete'),
-    url(r'^decide/$', 'openid_decide', name='openid-provider-decide'),
-    url(r'^xrds/$', 'openid_xrds', name='openid-provider-xrds'),
-    url(r'^(?P<id>.+)/xrds/$', 'openid_xrds', {'identity': True},
+    url(r'^decide/$',
+        views.openid_decide,
+        name='openid-provider-decide'),
+    url(r'^xrds/$',
+        views.openid_xrds,
+        name='openid-provider-xrds'),
+    url(r'^(?P<id>.+)/xrds/$',
+        views.openid_xrds,
+        {'identity': True},
         name='openid-provider-identity-xrds'),
-    url(r'^(?P<id>.+)/$', 'openid_discovery',
+    url(r'^(?P<id>.+)/$',
+        views.openid_discovery,
         name='openid-provider-identity'),
 )
