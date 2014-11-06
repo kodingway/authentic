db5014b5d9f7952ee73c484c27b9baa6ed9d6a57
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-11-06 10:56:09 +0100

    bump release to 2.1.8

--- authentic2/__init__.py
+++ authentic2/__init__.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-__version__ = "2.1.7"
+__version__ = "2.1.8"
 
 # vendor contains incorporated dependencies
 sys.path.append(os.path.join(os.path.dirname(__file__), 'vendor'))
