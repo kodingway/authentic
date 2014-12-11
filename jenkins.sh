bf4754d5b776a4d51790dca2b1a71e9dd88a953e
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-12-11 09:51:06 +0100

    Run tox as part of the continuous integration script, stop the script on any error
    refs #5244

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+set -e
+
 pip install --upgrade pip
 pip install --upgrade pylint==1.1.0
 pip install --upgrade -v -r requirements.txt
 pip install --upgrade django-authopenid
+pip install --upgrade tox
 export SECRET_KEY='coin'
 ./authentic2-ctl syncdb --noinput --all
 ./authentic2-ctl migrate --fake
 ./authentic2-ctl validate
 (pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc authentic2/ | tee pylint.out) || /bin/true
+tox
