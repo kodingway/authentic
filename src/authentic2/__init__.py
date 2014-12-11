6000feb90cc13ff27026939ffeba4b8c74209641
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-12-11 15:46:09 +0100

    [django-1.7] Use new application config ready() method to fix user models
    refs #5244

--- src/authentic2/__init__.py
+++ src/authentic2/__init__.py
@@ -1,7 +1,19 @@
 import sys
 import os
+import django
 
 __version__ = "2.1.11"
 
 # vendor contains incorporated dependencies
 sys.path.append(os.path.join(os.path.dirname(__file__), 'vendor'))
+
+if django.VERSION >= (1,7):
+    from django.apps import AppConfig
+
+    class Authentic2Config(AppConfig):
+        name = 'authentic2'
+        label = 'authentic2'
+
+        def ready(self):
+            from . import fix_user_model
+    default_app_config = 'authentic2.Authentic2Config'
