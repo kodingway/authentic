822ab9ab999b28ee73c713a546e05d3b6b0c1d45
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-12-17 12:03:17 +0100

    Use new helper methods in default login/password authentication frontend

--- authentic2/auth_frontends.py
+++ authentic2/auth_frontends.py
@@ -1,13 +1,8 @@
 from django.contrib.auth import forms
-from django.contrib.auth import REDIRECT_FIELD_NAME, login
 from django.utils.translation import gettext_noop
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.conf import settings
 
-from authentic2.constants import NONCE_FIELD_NAME
-
-from . import views, models, app_settings
+from . import views, app_settings, utils
 
 class LoginPasswordBackend(object):
     submit_name = 'login-password-submit'
@@ -23,25 +18,17 @@ class LoginPasswordBackend(object):
 
     def login(self, request, *args, **kwargs):
         context_instance = kwargs.get('context_instance', None)
-        next_url = request.GET.get(REDIRECT_FIELD_NAME) or settings.LOGIN_REDIRECT_URL
-        nonce = request.GET.get(NONCE_FIELD_NAME,'')
         is_post = request.method == 'POST' and self.submit_name in request.POST
         data = request.POST if is_post  else None
         form = forms.AuthenticationForm(data=data)
         is_secure = request.is_secure
 
         if is_post and form.is_valid():
-            # Login the user
-            login(request, form.get_user())
-            # Keep a trace
             if is_secure:
                 how = 'password-on-https'
             else:
                 how = 'password'
-            user = form.get_user()
-            models.AuthenticationEvent(who=unicode(user)[:80], how=how,
-                    nonce=nonce).save()
-            return HttpResponseRedirect(next_url)
+            return utils.login(request, form.get_user(), how)
         return render(request, 'authentic2/login_password_form.html', {
                 'submit_name': self.submit_name,
                 'form': form
