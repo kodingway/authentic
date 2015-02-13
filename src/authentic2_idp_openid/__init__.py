fcae7f2067574cec1d4a8baf5c17614c80b00744
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2015-02-13 15:16:17 +0100

    OpenID IdP: raise ImproperlyConfigured if it is enabled but python-openid is not installed

--- src/authentic2_idp_openid/__init__.py
+++ src/authentic2_idp_openid/__init__.py
@@ -1,34 +1,44 @@
 from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ImproperlyConfigured
 
-class Plugin(object):
-    def get_before_urls(self):
-        from . import app_settings
-        from django.conf.urls import patterns, include
-        from authentic2.decorators import (setting_enabled, required,
-                lasso_required)
+try:
+    import openid
+except ImportError:
+    from . import app_settings
+    if app_settings.ENABLE:
+        raise ImproperlyConfigured('OpenID idp is enabled by python-openid is not installed')
+    class Plugin(object):
+        pass
+else:
+    class Plugin(object):
+        def get_before_urls(self):
+            from . import app_settings
+            from django.conf.urls import patterns, include
+            from authentic2.decorators import (setting_enabled, required,
+                    lasso_required)
 
-        return required(
-                (
-                    setting_enabled('ENABLE', settings=app_settings),
-                    lasso_required()
+            return required(
+                    (
+                        setting_enabled('ENABLE', settings=app_settings),
+                        lasso_required()
+                    ),
+                    patterns('',
+                        (r'^idp/openid/', include(__name__ + '.urls'))))
+
+        def get_apps(self):
+            return [__name__]
+
+        def get_admin_modules(self):
+            from admin_tools.dashboard import modules
+            return [modules.ModelList(
+                _('OpenID'),
+                models=(
+                    '%s.models.*' % __name__,
                 ),
-                patterns('',
-                    (r'^idp/openid/', include(__name__ + '.urls'))))
+            )]
 
-    def get_apps(self):
-        return [__name__]
+        def get_idp_backends(self):
+            return ['%s.backend.OpenIDBackend' % __name__]
 
-    def get_admin_modules(self):
-        from admin_tools.dashboard import modules
-        return [modules.ModelList(
-            _('OpenID'),
-            models=(
-                '%s.models.*' % __name__,
-            ),
-        )]
-
-    def get_idp_backends(self):
-        return ['%s.backend.OpenIDBackend' % __name__]
-
-    def get_before_middleware(self):
-        return ['%s.middleware.OpenIDMiddleware' % __name__]
+        def get_before_middleware(self):
+            return ['%s.middleware.OpenIDMiddleware' % __name__]
