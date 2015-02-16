d6156bcf87709e9e1cc0b151e1f0b9950e5d347b
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2015-02-16 01:34:05 +0100

    At least install tox and pylnt

--- jenkins.sh
+++ jenkins.sh
@@ -2,5 +2,8 @@
 
 set -e
 
+pip install --upgrade pip
+pip install --upgrade pylint==1.1.0
+pip install --upgrade tox
 (pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc authentic2/ | tee pylint.out) || /bin/true
 tox
