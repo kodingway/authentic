358a8e7a7ef15637d95f68c14fe22e88dcd2821c
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2015-02-16 01:24:11 +0100

    Just use tox

--- jenkins.sh
+++ jenkins.sh
@@ -2,14 +2,5 @@
 
 set -e
 
-pip install --upgrade pip
-pip install --upgrade pylint==1.1.0
-pip install --upgrade django-authopenid
-pip install --upgrade tox
-pip install --upgrade .
-export SECRET_KEY='coin'
-./authentic2-ctl syncdb --noinput --all
-./authentic2-ctl migrate --fake
-./authentic2-ctl validate
 (pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc authentic2/ | tee pylint.out) || /bin/true
 tox
