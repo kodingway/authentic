b3be6b3aad169ab25449444c7745a8b732756c4f
Serghei MIHAI <smihai@entrouvert.com>
2015-02-19 15:57:17 +0100

    multitenant: packaging refactored according to our packagind guide

---
+++ multitenant/config.py
@@ -0,0 +1,41 @@
+# Here your custom configuration.
+# Use it to declare new settings or overload existing ones
+
+# STATIC_ROOT='/var/lib/authentic2/static'
+
+# ALLOWED_HOSTS='*'
+
+# SAML_SIGNATURE_PUBLIC_KEY=""
+# SAML_SIGNATURE_PRIVATE_KEY=""
+
+# USE_MEMCACHED=yes # required python-memcache memcached
+
+# IDP_SAML2='yes'
+# IDP_OPENID='yes' # require package python-openid
+# IDP_CAS='yes'
+# AUTH_SAML2='yes'
+# AUTH_OPENID='yes' # require package python-openid
+# AUTH_SSL='yes'
+
+# Sentry / Raven configuration
+# RAVEN_CONFIG_DSN='' # require package  python-raven
+
+# Email configuration
+# EMAIL_HOST = 'localhost'
+# EMAIL_PORT = 25
+# EMAIL_SUBJECT_PREFIX='[Authentic2]'
+# SERVER_EMAIL='admin+authentic2@entrouvert.com'
+
+# complete database config
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'tenant_schemas.postgresql_backend',
+#         'HOST': 'localhost',
+#         'NAME':  'authentic',
+#         'PASSWORD': 'secret',
+#         'PORT': 5432
+#     }
+# }
+# or each param separately
+# DATABASES['default']['HOST'] = 'another host'
+# DATABASES['default']['PASSWORD'] = 'another secret'
---
+++ multitenant/debian_config.py
@@ -0,0 +1,59 @@
+import os
+
+from django.core.exceptions import ImproperlyConfigured
+
+PROJECT_NAME = 'authentic2-multitenant'
+
+try:
+    import hobo
+except ImportError:
+    raise ImproperlyConfigured('hobo MUST be installed for the multitenant mode to work')
+
+VAR_DIR = os.path.join('/var/lib', PROJECT_NAME)
+ETC_DIR = os.path.join('/etc', PROJECT_NAME)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+TEMPLATE_DEBUG = False
+
+TENANT_MODEL = 'multitenant.Tenant'
+TENANT_BASE = os.path.join(VAR_DIR, 'tenants')
+TENANT_TEMPLATE_DIRS = (TENANT_BASE,)
+
+SHARED_APPS = (
+    'hobo.multitenant',
+    'django.contrib.staticfiles',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+)
+
+TENANT_APPS = INSTALLED_APPS
+
+INSTALLED_APPS = ('hobo.multitenant', 'hobo.agent.authentic2') + INSTALLED_APPS
+
+TEMPLATE_LOADERS = ('hobo.multitenant.template_loader.FilesystemLoader',) + TEMPLATE_LOADERS
+
+TEMPLATE_CONTEXT_PROCESSORS = ('django.core.context_processors.request',) + TEMPLATE_CONTEXT_PROCESSORS
+
+MIDDLEWARE_CLASSES = (
+    'hobo.multitenant.middleware.TenantMiddleware',
+    'hobo.middleware.settings.AuthenticSettingsMiddleware'
+) + MIDDLEWARE_CLASSES
+
+DEFAULT_FILE_STORAGE = 'hobo.multitenant.storage.TenantFileSystemStorage'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'tenant_schemas.postgresql_backend',
+        'NAME': PROJECT_NAME.replace('-', '_')
+    }
+}
+
+DATABASE_ROUTERS = (
+    'tenant_schemas.routers.TenantSyncRouter',
+)
+
+if os.path.exists(os.path.join(ETC_DIR, 'config.py')):
+    execfile(os.path.join(ETC_DIR, 'config.py'))
