034e55ef987345c06d180050dc27c5e674c16e93
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2015-03-31 17:02:33 +0200

    Add a merge migration to the saml application

---
+++ src/authentic2/saml/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('saml', '0002_auto_20150320_1245'),
+        ('saml', '0002_ease_federation_migration'),
+    ]
+
+    operations = [
+    ]
