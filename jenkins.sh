cf76bbc8c4619f4516542123283ba61322b8470e
Frédéric Péters <fpeters@entrouvert.com>
2015-04-13 10:00:45 +0200

    jenkins: update pylint to look in src/

--- jenkins.sh
+++ jenkins.sh
@@ -5,5 +5,5 @@ set -e
 pip install --upgrade pip
 pip install --upgrade pylint==1.4.0 astroid==1.3.2
 pip install --upgrade tox
-(pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc authentic2/ | tee pylint.out) || /bin/true
+(pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc src/ | tee pylint.out) || /bin/true
 tox
