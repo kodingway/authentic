23fdbbf3748d3e4b44875fdb42acb9b2a49c8f48
Frédéric Péters <fpeters@entrouvert.com>
2015-04-13 09:41:51 +0200

    jenkins: update to pylint 1.4.0

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@
 set -e
 
 pip install --upgrade pip
-pip install --upgrade pylint==1.1.0
+pip install --upgrade pylint==1.4.0 astroid==1.3.2
 pip install --upgrade tox
 (pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc authentic2/ | tee pylint.out) || /bin/true
 tox
