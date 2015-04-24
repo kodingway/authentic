118d90424e6e193d57d01e071ccc77d89b4c422f
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2015-04-24 12:07:55 +0200

    manager: add uuid to user table's columns

--- src/authentic2/manager/tables.py
+++ src/authentic2/manager/tables.py
@@ -6,6 +6,7 @@ import django_tables2 as tables
 from authentic2.compat import get_user_model
 
 class UserTable(tables.Table):
+    uuid = tables.TemplateColumn('{{ record.uuid|slice:":6" }}', verbose_name=_('UUID'))
     username = tables.TemplateColumn(
         '<a rel="popup" href="{% url "a2-manager-user-edit" pk=record.pk %}">{{ record.username }}</a>',
         verbose_name=_('username'))
@@ -14,6 +15,6 @@ class UserTable(tables.Table):
     class Meta:
         model = get_user_model()
         attrs = {'class': 'main', 'id': 'user-table'}
-        fields = ('username', 'email', 'first_name', 'last_name',
+        fields = ('uuid', 'username', 'email', 'first_name', 'last_name',
                 'is_active')
         empty_text = _('None')
