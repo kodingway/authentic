8453e271330c8ba9d12f24a0e98e9cd908433318
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2015-04-27 01:00:40 +0200

    fix appconfig path

--- src/authentic2/__init__.py
+++ src/authentic2/__init__.py
@@ -4,4 +4,4 @@ import os
 # vendor contains incorporated dependencies
 sys.path.append(os.path.join(os.path.dirname(__file__), 'vendor'))
 
-default_app_config = 'authentic2.Authentic2Config'
+default_app_config = 'authentic2.apps.Authentic2Config'
