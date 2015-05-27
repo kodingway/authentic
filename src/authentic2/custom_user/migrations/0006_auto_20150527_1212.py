eda129d7b23fc2cc6a4c90844fa71f948cb725bb
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2015-05-27 15:33:08 +0200

    custom_user: add missing migration (fixes #7150)

---
+++ src/authentic2/custom_user/migrations/0006_auto_20150527_1212.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('custom_user', '0005_auto_20150522_1527'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='user',
+            name='last_login',
+            field=models.DateTimeField(null=True, verbose_name='last login', blank=True),
+        ),
+    ]
