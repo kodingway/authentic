kodingway/authentic

commit 7141a96e62ae10abad92980d04050fd1892282e4

forms: fix modelform_factory for non-user models
fixes #7249

src/authentic2/forms.py
@@ -81,12 +81,14 @@ def modelform_factory(model, **kwargs):
 
        For the user model also add attribute based fields.
     '''
-    form = kwargs.pop('form', None)
+    form = kwargs.pop('form', forms.ModelForm)
     fields = kwargs.get('fields', [])
     required = list(kwargs.pop('required', []))
-    d = {'model': model, 'fields': '__all__'}
-    meta = type('Meta', (), d)
-    d = {'Meta': meta}
+    d = {}
+    if not form or not hasattr(form, 'Meta'):
+        meta_d = {'model': model, 'fields': '__all__'}
+        meta = type('Meta', (), meta_d)
+        d['Meta'] = meta
     bases = (form,)
     # KV attributes are only supported for the user model currently
     modelform = None

commit 6786b2529d35f81d8c90068871a24db4186f93db

forms: if not form class is given to modelform_factory() on User model, user BaseUserForm (fixes #7391)
Building of the base classe tuple has been refactoed.

Bug introduced in 7141a96e6

src/authentic2/forms.py
@@ -76,27 +76,21 @@ class BaseUserForm(forms.ModelForm):
             self.save_m2m = save_m2m
         return result
 
+
 def modelform_factory(model, **kwargs):
     '''Build a modelform for the given model,
 
        For the user model also add attribute based fields.
     '''
-    form = kwargs.pop('form', None) or forms.ModelForm
+    form = kwargs.pop('form', None)
     fields = kwargs.get('fields', [])
     required = list(kwargs.pop('required', []))
     d = {}
-    if not form or not hasattr(form, 'Meta'):
-        meta_d = {'model': model, 'fields': '__all__'}
-        meta = type('Meta', (), meta_d)
-        d['Meta'] = meta
-    bases = (form,)
     # KV attributes are only supported for the user model currently
     modelform = None
     if model == get_user_model():
-        if form:
-            bases = (form,)
-        else:
-            bases = (BaseUserForm,)
+        if not form:
+            form = BaseUserForm
         attributes = models.Attribute.objects.all()
         for attribute in attributes:
             if fields and attribute.name not in fields:
@@ -105,14 +99,20 @@ def modelform_factory(model, **kwargs):
         for field in app_settings.A2_REQUIRED_FIELDS:
             if not field in required:
                 required.append(field)
+    if not form or not hasattr(form, 'Meta'):
+        meta_d = {'model': model, 'fields': '__all__'}
+        meta = type('Meta', (), meta_d)
+        d['Meta'] = meta
+    if not form:  # fallback
+        form = forms.ModelForm
     modelform = None
     if required:
         def __init__(self, *args, **kwargs):
             super(modelform, self).__init__(*args, **kwargs)
             for field in required:
                 if field in self.fields:
-		    self.fields[field].required = True
+                    self.fields[field].required = True
         d['__init__'] = __init__
-    modelform = type(model.__name__ + 'ModelForm', bases, d)
+    modelform = type(model.__name__ + 'ModelForm', (form,), d)
     kwargs['form'] = modelform
     return django_modelform_factory(model, **kwargs)