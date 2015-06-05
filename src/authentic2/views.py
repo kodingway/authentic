7c9c0e50d490601f4837e4dcb1223a1dd19b85bd
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2015-06-05 14:54:49 +0200

    views: make redirection to A2_HOMEPAGE_URL transient
    With permanent it can never be changed.

--- src/authentic2/views.py
+++ src/authentic2/views.py
@@ -105,7 +105,7 @@ def su(request, username, redirect_url='/'):
 
 
 class RedirectToHomepageView(RedirectView):
-    permanent = True
+    permanent = False
     url = app_settings.A2_HOMEPAGE_URL
 
 
