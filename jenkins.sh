5405878a8d9a4b6251b8772819c5c75ec534f4bb
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2015-06-18 16:20:27 +0200

    Always recreate tox environment

--- jenkins.sh
+++ jenkins.sh
@@ -7,4 +7,4 @@ pip install --upgrade pip
 pip install --upgrade pylint==1.4.0 astroid==1.3.2
 pip install --upgrade tox
 (pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc src/authentic2/ | tee pylint.out) || /bin/true
-tox
+tox -r
