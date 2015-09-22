8f81aa8903c34ae142a118650df6d4c8a9e0daeb
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2015-09-22 22:56:07 +0200

    jenkins: add scripts to merge coverage and junit reports produced by pytest

--- jenkins.sh
+++ jenkins.sh
@@ -8,3 +8,5 @@ pip install --upgrade pylint==1.4.0 astroid==1.3.2
 pip install --upgrade tox
 (pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc src/authentic2/ | tee pylint.out) || /bin/true
 tox -r
+./merge-junit-results.py rbac-django17.xml django17.xml  >junit.xml
+./merge-coverage.py -o coverage.xml *coverage.xml
