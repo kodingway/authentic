0e2b719f857991a1d38dc0eaf5208ae6638f9210
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2015-11-10 10:32:15 +0100

    api_urls: fix regexs (#7862)

--- src/authentic2/api_urls.py
+++ src/authentic2/api_urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, url
 from . import api_views
 
 urlpatterns = patterns('',
-                       url(r'register/$', api_views.register,
+                       url(r'^register/$', api_views.register,
                            name='a2-api-register'),
-                       url(r'password-change/$', api_views.password_change,
+                       url(r'^password-change/$', api_views.password_change,
                            name='a2-api-password-change'),
                        url(r'^user/$', api_views.user,
                            name='a2-api-user'),
