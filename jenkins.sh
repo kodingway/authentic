51671d4829d339de70099acb86fe59fd69157e52
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2016-02-03 18:17:25 +0100

    jenkins.sh: use pip<8

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@
 set -e
 
 ./getlasso.sh
-pip install --upgrade pip
+pip install --upgrade 'pip<8'
 pip install --upgrade pylint==1.4.0 astroid==1.3.2
 pip install --upgrade tox
 (pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc src/authentic2/ | tee pylint.out) || /bin/true
