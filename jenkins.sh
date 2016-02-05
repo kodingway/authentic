17f2d1217bdaeb399d8e3625aa1b5d4ea13d2e8b
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2016-02-05 13:47:53 +0100

    jekins.sh: try something else

--- jenkins.sh
+++ jenkins.sh
@@ -5,10 +5,10 @@ set -e
 ./getlasso.sh
 # SNI support for Debian
 pip install --upgrade pyOpenSSL ndg-httpsclient pyasn1
-pip install --upgrade pip
+pip install --upgrade pip setuptools wheel
 pip install --upgrade pylint==1.4.0 astroid==1.3.2
 pip install --upgrade tox
-(pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc src/authentic2/ | tee pylint.out) || /bin/true
-tox -r
+# (pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc src/authentic2/ | tee pylint.out) || /bin/true
+tox -r -v
 ./merge-junit-results.py rbac-django17.xml django17.xml  >junit.xml
 ./merge-coverage.py -o coverage.xml *coverage.xml
