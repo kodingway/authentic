1b06c3b3a40886378ff431e4bb832d77da098bdf
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2016-02-11 22:42:26 +0100

    support any where clause in CreatePartialIndexes (#9947)

--- src/authentic2/migrations/__init__.py
+++ src/authentic2/migrations/__init__.py
@@ -7,16 +7,21 @@ from django.db.migrations.operations.base import Operation
 class CreatePartialIndexes(Operation):
     reversible = True
 
-    def __init__(self, model_name, table_name, index_name, nullable_columns, non_null_columns, null_columns=()):
+    def __init__(self, model_name, table_name, index_name, nullable_columns, non_null_columns,
+                 null_columns=None, where=None):
         self.model_name = model_name
         self.table_name = table_name
         self.index_name = index_name
+        null_columns = null_columns or ()
         assert not set(nullable_columns) & set(non_null_columns)
         assert not set(null_columns) & set(non_null_columns)
         assert not set(null_columns) & set(nullable_columns)
         self.nullable_columns = set(nullable_columns)
         self.non_null_columns = set(non_null_columns)
-        self.null_columns = set(null_columns)
+        self.where = set(where or [])
+        if null_columns:
+            for column in null_columns:
+                self.where.add('"%s" IS NULL' % column)
 
     def allowed(self, app_label, schema_editor, to_state):
         if django.VERSION < (1, 8, 0):
@@ -39,9 +44,11 @@ class CreatePartialIndexes(Operation):
         for i in range(0, len(self.nullable_columns)+1):
             for null_columns in itertools.combinations(sorted(self.nullable_columns), i):
                 non_null_columns = self.non_null_columns | (self.nullable_columns - set(null_columns))
-                null_columns = set(null_columns) | self.null_columns
+                where = self.where.copy()
+                for column in null_columns:
+                    where.add('"%s" IS NULL' % column)
                 non_null_columns = sorted(non_null_columns)
-                yield null_columns, non_null_columns
+                yield where, non_null_columns
 
     def state_forwards(self, app_label, state):
         pass
@@ -49,11 +56,12 @@ class CreatePartialIndexes(Operation):
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
         if not self.allowed(app_label, schema_editor, to_state):
             return
-        for i, (null_columns, non_null_columns) in enumerate(self.indexes()):
+        for i, (where, non_null_columns) in enumerate(self.indexes()):
             index = ', '.join(non_null_columns)
-            if null_columns:
-                where = ' AND '.join('"%s" IS NULL' % col for col in null_columns)
-                schema_editor.execute('CREATE UNIQUE INDEX "%s_%s" ON %s (%s) WHERE %s' % (self.index_name, i, self.table_name, index, where))
+            if where:
+                where_clause = ' AND '.join(where)
+                schema_editor.execute('CREATE UNIQUE INDEX "%s_%s" ON %s (%s) WHERE %s' %
+                                      (self.index_name, i, self.table_name, index, where_clause))
             else:
                 schema_editor.execute('CREATE UNIQUE INDEX "%s_%s" ON %s (%s)' % (self.index_name, i, self.table_name, index))
 
