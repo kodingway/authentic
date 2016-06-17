d25cef3383a73a6a3eb3544dff62d9e09ea014f6
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2016-06-17 13:25:50 +0200

    trivial: PEP8 formatting

--- src/authentic2/attribute_kinds.py
+++ src/authentic2/attribute_kinds.py
@@ -21,25 +21,26 @@ DEFAULT_TITLE_CHOICES = (
     (pgettext_lazy('title', 'Mr'), pgettext_lazy('title', 'Mr')),
 )
 
+
 @to_iter
 def get_title_choices():
     return app_settings.A2_ATTRIBUTE_KIND_TITLE_CHOICES or DEFAULT_TITLE_CHOICES
 
 DEFAULT_ATTRIBUTE_KINDS = [
-        {
-          'label': _('string'),
-          'name': 'string',
-          'field_class': forms.CharField,
-        },
-        {
-          'label': _('title'),
-          'name': 'title',
-          'field_class': forms.ChoiceField,
-          'kwargs': {
-               'choices': get_title_choices(),
-               'widget': forms.RadioSelect,
-          }
+    {
+        'label': _('string'),
+        'name': 'string',
+        'field_class': forms.CharField,
+    },
+    {
+        'label': _('title'),
+        'name': 'title',
+        'field_class': forms.ChoiceField,
+        'kwargs': {
+            'choices': get_title_choices(),
+            'widget': forms.RadioSelect,
         }
+    },
 ]
 
 
@@ -58,37 +59,43 @@ def get_choices():
     for d in get_attribute_kinds().itervalues():
         yield (d['name'], capfirst(d['label']))
 
+
 def only_digits(value):
     return u''.join(x for x in value if x in string.digits)
 
+
 def validate_lun(value):
-    l = [(int(x)* (1+i%2)) for i, x in enumerate(reversed(value))]
-    return sum(x-9 if x > 10 else x for x in l) % 10 == 0
+    l = [(int(x) * (1 + i % 2)) for i, x in enumerate(reversed(value))]
+    return sum(x - 9 if x > 10 else x for x in l) % 10 == 0
+
 
 def validate_siret(value):
-    RegexValidator(r'^( *[0-9] *){14}$', 
-            _('SIRET number must contain 14 digits'), 'coin')(value)
+    RegexValidator(r'^( *[0-9] *){14}$', _('SIRET number must contain 14 digits'), 'coin')(value)
     value = only_digits(value)
     if not validate_lun(value) or not validate_lun(value[:9]):
         raise ValidationError(_('SIRET validation code does not match'))
 
+
 class SIRETField(forms.CharField):
-    default_validators = [ validate_siret ]
+    default_validators = [validate_siret]
 
     def to_python(self, value):
         value = super(SIRETField, self).to_python(value)
         value = only_digits(value)
         return value
 
+
 def contribute_to_form(attribute_descriptions, form):
     for attribute_description in attribute_descriptions:
         attribute_description.contribute_to_form(form)
+
 
 def get_form_field(kind, **kwargs):
     defn = get_attribute_kinds()[kind]
     if 'kwargs' in defn:
         kwargs.update(defn['kwargs'])
     return defn['field_class'](**kwargs)
+
 
 def get_kind(kind):
     d = get_attribute_kinds()[kind]
