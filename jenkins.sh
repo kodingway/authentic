002eefe475f07428a575cbadac78ba4a55ab93e7
Frédéric Péters <fpeters@entrouvert.com>
2016-08-27 20:20:48 +0200

    jenkins: fix coverage support (#12970)

--- jenkins.sh
+++ jenkins.sh
@@ -9,7 +9,8 @@ pip install --upgrade pip
 pip install --upgrade pylint pylint-django
 pip install --upgrade tox
 pip install -U 'virtualenv<14'
+rm -f coverage*.xml
 tox -r -e 'fast-coverage-{dj17,dj18}-{authentic,rbac}-{pg,sqlite}'
 (pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc src/authentic2/ | tee pylint.out) || /bin/true
 ./merge-junit-results.py rbac-django17.xml django17.xml  >junit.xml
-./merge-coverage.py -o coverage.xml *coverage.xml
+./merge-coverage.py -o coverage.xml coverage-*.xml
