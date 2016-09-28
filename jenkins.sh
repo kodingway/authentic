db38e96409d82d062fe19db8252d49fbf9981ce9
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2016-09-28 11:36:06 +0200

    jenkins.sh: run tests with migrations at night

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -10,7 +10,12 @@ pip install --upgrade pylint pylint-django
 pip install --upgrade tox
 pip install -U 'virtualenv<14'
 rm -f coverage*.xml
-tox -r -e 'fast-coverage-{dj17,dj18}-{authentic,rbac}-{pg,sqlite}'
+if [[ `date +%H%M` < 630 ]]; then
+	# run full tests (with migrations) at night
+	tox -r -e 'coverage-{dj17,dj18}-{authentic,rbac}-{pg,sqlite}'
+else
+	tox -r -e 'fast-coverage-{dj17,dj18}-{authentic,rbac}-{pg,sqlite}'
+fi
 (pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc src/authentic2/ | tee pylint.out) || /bin/true
 ./merge-junit-results.py junit-fast-coverage-dj18-authentic-pg.xml junit-fast-coverage-dj18-rbac-pg.xml > junit.xml
 ./merge-coverage.py -o coverage.xml coverage-*.xml
