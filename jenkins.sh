287c3a1e376bf65ce42fdb21d880339cee34d4e2
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2016-11-06 23:16:36 +0100

    jenkins.sh: remove junit files before each run

--- jenkins.sh
+++ jenkins.sh
@@ -9,7 +9,7 @@ pip install --upgrade pip
 pip install --upgrade pylint pylint-django
 pip install --upgrade tox
 pip install -U 'virtualenv<14'
-rm -f coverage*.xml
+rm -f coverage*.xml junit*.xml
 if [[ `date +%H%M` < 630 ]]; then
 	# run full tests (with migrations) at night
 	tox -r -e 'coverage-{dj17,dj18}-{authentic,rbac}-{pg,sqlite}'
@@ -17,5 +17,5 @@ else
 	tox -r -e 'fast-coverage-{dj17,dj18}-{authentic,rbac}-{pg,sqlite}'
 fi
 (pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc src/authentic2/ | tee pylint.out) || /bin/true
-./merge-junit-results.py junit-fast-coverage-dj18-authentic-pg.xml junit-fast-coverage-dj18-rbac-pg.xml > junit.xml
+./merge-junit-results.py junit-*coverage-dj18-authentic-pg.xml junit-*coverage-dj18-rbac-pg.xml > junit.xml
 ./merge-coverage.py -o coverage.xml coverage-*.xml
