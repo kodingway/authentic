480c2374606c85102169615e6e40a7835d17b17a
Frédéric Péters <fpeters@entrouvert.com>
2017-03-15 23:17:43 +0100

    jenkins: remove django 1.7 support

--- jenkins.sh
+++ jenkins.sh
@@ -12,9 +12,9 @@ pip install -U 'virtualenv<14'
 rm -f coverage*.xml junit*.xml
 if [[ `date +%H%M` < 630 ]]; then
 	# run full tests (with migrations) at night
-	tox -r -e 'coverage-{dj17,dj18}-{authentic,rbac}-{pg,sqlite}'
+	tox -r -e 'coverage-dj18-{authentic,rbac}-{pg,sqlite}'
 else
-	tox -r -e 'fast-coverage-{dj17,dj18}-{authentic,rbac}-{pg,sqlite}'
+	tox -r -e 'fast-coverage-dj18-{authentic,rbac}-{pg,sqlite}'
 fi
 (pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc src/authentic2/ | tee pylint.out) || /bin/true
 ./merge-junit-results.py junit-*coverage-dj18-authentic-pg.xml junit-*coverage-dj18-rbac-pg.xml > junit.xml
