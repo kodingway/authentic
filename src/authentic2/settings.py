7f8df78bf506416a2618b1b5b476ddba2614a5a2
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2017-04-04 10:19:14 +0200

    api: augment default pagination page size to 100 (fixes #15622)
    Pagination was made mandatory by issue #15620, and page size was 10 which is too
    small.

--- src/authentic2/settings.py
+++ src/authentic2/settings.py
@@ -283,7 +283,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 100,
 }
 
 
