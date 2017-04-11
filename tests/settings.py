1e64f411aa2ba271a98bd88d94a54db6fe6feac2
Josue Kouka <jkouka@entrouvert.com>
2017-04-11 12:03:00 +0200

    tests: use sqlite3 backend if none defined in env

--- tests/settings.py
+++ tests/settings.py
@@ -3,7 +3,7 @@ import os
 LANGUAGE_CODE = 'en'
 DATABASES = {
     'default': {
-        'ENGINE': os.environ['DB_ENGINE'],
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
         'TEST': {
             'NAME': 'a2-test',
         },
