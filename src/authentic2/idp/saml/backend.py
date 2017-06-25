7fddf2ebfea77fc08d921557fd4ecb7104952ec9
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2017-06-25 11:53:39 +0200

    idp_saml: pep8ness

--- src/authentic2/idp/saml/backend.py
+++ src/authentic2/idp/saml/backend.py
@@ -13,7 +13,7 @@ import authentic2.idp.saml.saml2_endpoints as saml2_endpoints
 import authentic2.saml.common as common
 
 from authentic2.decorators import to_list
-from authentic2.utils import Service, ServiceAccessDenied
+from authentic2.utils import Service
 
 
 class SamlBackend(object):
@@ -21,12 +21,13 @@ class SamlBackend(object):
         self.logger = logging.getLogger(__name__)
 
     def service_list(self, request):
-        q = models.LibertyServiceProvider.objects.filter(enabled = True) \
-                .select_related()
-        q = q.filter( Q(liberty_provider__authorized_roles__isnull=True) | Q(liberty_provider__authorized_roles__in=request.user.roles_and_parents()))
+        q = models.LibertyServiceProvider.objects.all()
+        q = q.filter(
+            Q(liberty_provider__authorized_roles__isnull=True)
+            | Q(liberty_provider__authorized_roles__in=request.user.roles_and_parents()))
         ls = []
         sessions = models.LibertySession.objects.filter(
-                django_session_key=request.session.session_key)
+            django_session_key=request.session.session_key)
         sessions_eids = set(session.provider_id for session in sessions)
         all_policy = common.get_sp_options_policy_all()
         default_policy = common.get_sp_options_policy_default()
@@ -35,16 +36,23 @@ class SamlBackend(object):
             queries.append(q)
             queries.append(q.filter(liberty_provider__entity_id__in=sessions_eids))
         else:
-            queries.append(q.filter(sp_options_policy__enabled=True,
+            queries.append(q.filter(
+                sp_options_policy__enabled=True,
                 sp_options_policy__idp_initiated_sso=True))
-            queries.append(q.filter(sp_options_policy__enabled=True,
-                sp_options_policy__accept_slo=True,
-                liberty_provider__entity_id__in=sessions_eids))
-            if default_policy and default_policy.idp_initiated_sso:
-                queries.append(q.filter(sp_options_policy__isnull=True))
-            if default_policy and default_policy.accept_slo:
-                queries.append(q.filter(sp_options_policy__isnull=True,
+            queries.append(
+                q.filter(
+                    sp_options_policy__enabled=True,
+                    sp_options_policy__accept_slo=True,
                     liberty_provider__entity_id__in=sessions_eids))
+            if default_policy and default_policy.idp_initiated_sso:
+                queries.append(
+                    q.filter(
+                        sp_options_policy__isnull=True))
+            if default_policy and default_policy.accept_slo:
+                queries.append(
+                    q.filter(
+                        sp_options_policy__isnull=True,
+                        liberty_provider__entity_id__in=sessions_eids))
         qs = reduce(operator.__or__, queries)
         # do some prefetching
         qs = qs.prefetch_related('liberty_provider')
@@ -63,21 +71,33 @@ class SamlBackend(object):
                 entity_id = liberty_provider.entity_id
                 protocol = 'saml2'
                 if policy.idp_initiated_sso:
-                    actions.append(('login', 'POST',
-                        '/idp/%s/idp_sso/' % protocol,
-                        (('provider_id', entity_id ),)))
+                    actions.append(
+                        (
+                            'login', 'POST',
+                            '/idp/%s/idp_sso/' % protocol,
+                            (
+                                ('provider_id', entity_id),
+                            )
+                        )
+                    )
                 if policy.accept_slo and entity_id in sessions_eids:
-                    actions.append(('logout', 'POST',
-                        '/idp/%s/idp_slo/' % protocol,
-                        (( 'provider_id', entity_id ),)))
+                    actions.append(
+                        (
+                            'logout', 'POST',
+                            '/idp/%s/idp_slo/' % protocol,
+                            (
+                                ('provider_id', entity_id),
+                            )
+                        )
+                    )
                 if actions:
-                    ls.append(Service(url=None, name=liberty_provider.name,
-                        actions=actions))
+                    ls.append(
+                        Service(url=None, name=liberty_provider.name, actions=actions))
         return ls
 
     def logout_list(self, request):
         all_sessions = models.LibertySession.objects.filter(
-                django_session_key=request.session.session_key)
+            django_session_key=request.session.session_key)
         self.logger.debug("all_sessions %r" % all_sessions)
         provider_ids = set([s.provider_id for s in all_sessions])
         self.logger.debug("provider_ids %r" % provider_ids)
@@ -89,15 +109,13 @@ class SamlBackend(object):
                 provider = models.LibertyProvider.objects.get(entity_id=provider_id)
                 name = provider.name
             except models.LibertyProvider.DoesNotExist:
-                self.logger.error('session found for unknown provider %s' \
-                    % provider_id)
+                self.logger.error(u'session found for unknown provider %s', provider_id)
             else:
                 policy = common.get_sp_options_policy(provider)
                 if not policy:
-                    self.logger.error('No policy found for %s' % provider_id)
+                    self.logger.error(u'No policy found for %s', provider_id)
                 elif not policy.forward_slo:
-                    self.logger.info('%s configured to not reveive slo' \
-                        % provider_id)
+                    self.logger.info(u'%s configured to not reveive slo', provider_id)
                 else:
                     url = reverse(saml2_endpoints.idp_slo, args=[provider_id])
                     # add a nonce so this link is never cached
@@ -117,13 +135,14 @@ class SamlBackend(object):
             return ()
         user = request.user
         links = []
-        qs = models.LibertyFederation.objects \
-                .filter(user=user,
-                        sp__isnull=False) \
-                .select_related('sp')
+        qs = models.LibertyFederation.objects.filter(
+            user=user,
+            sp__isnull=False)
+        qs = qs.select_related('sp')
         for federation in qs:
-            links.append((federation.sp.liberty_provider,
-                federation.name_id_content))
+            links.append(
+                (federation.sp.liberty_provider, federation.name_id_content)
+            )
         return links
 
     def can_synchronous_logout(self, django_sessions_keys):
@@ -136,30 +155,34 @@ class SamlBackend(object):
         qs = qs.filter(user=request.user)
         federations = qs.select_related()
         next_url = request.get_full_path()
-        for federation in  federations:
-            url = reverse('a2-idp-saml2-federation-delete',
-                    kwargs={'pk': federation.pk})
+        for federation in federations:
+            url = reverse('a2-idp-saml2-federation-delete', kwargs={'pk': federation.pk})
             yield {
-                    'name': federation.sp.liberty_provider.name,
-                    'hidden_inputs': {
-                        'next': next_url,
-                    },
-                    'buttons': (('delete', _('Delete')),),
-                    'url': url,
-                  }
+                'name': federation.sp.liberty_provider.name,
+                'hidden_inputs': {
+                    'next': next_url,
+                },
+                'buttons': (
+                    ('delete', _('Delete')),
+                ),
+                'url': url,
+            }
         qs = models.LibertyProvider.objects
         qs = qs.filter(service_provider__users_can_manage_federations=True)
         qs = qs.exclude(service_provider__libertyfederation__in=federations)
-        qs = qs.filter(Q(authorized_roles__isnull=True)|Q(authorized_roles__in=request.user.roles_and_parents()))
+        qs = qs.filter(Q(authorized_roles__isnull=True)
+                       | Q(authorized_roles__in=request.user.roles_and_parents()))
         qs = qs.select_related()
         for liberty_provider in qs:
             url = reverse('a2-idp-saml2-idp-sso')
             yield {
-                    'name': liberty_provider.name,
-                    'hidden_inputs': {
-                        'provider_id': liberty_provider.entity_id,
-                        'next': next_url,
-                    },
-                    'buttons': (('create', _('Create')),),
-                    'url': url,
-                  }
+                'name': liberty_provider.name,
+                'hidden_inputs': {
+                    'provider_id': liberty_provider.entity_id,
+                    'next': next_url,
+                },
+                'buttons': (
+                    ('create', _('Create')),
+                ),
+                'url': url,
+            }
