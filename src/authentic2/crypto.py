ce60f171967294289792acf78fbf39cb8ab8717f
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2017-07-11 11:38:22 +0200

    crypto: add deterministic encryption (fixes #17537)
    Data is encrypted using a key, a fixed salt and hash of the data.
    
    Salt is hashed with a chosen hash algorithm. The same hash algorithm is used to
    hash datas.
    
    The key is improved using PBKDF2. by default with a count of 1 and same salt
    used for generating the IV for encryption.
    
    IV is generated by xoring hash of the salt with hash of the data.
    
    Transmitted hash is shortened to key size of the used algorithm (here AES128).
    
    Count, name of the hash algorithm, base64url encoded value of the hash of the
    data and base64url encoding of the ciphered text are joined by a dollar
    character.
    
    Deterministic encryption is useful to generate reversible, targeted, opaque to
    third parties pseudonymous such as OpenID Connect sub or SAML NameID or
    SessionIndex.

--- src/authentic2/crypto.py
+++ src/authentic2/crypto.py
@@ -1,12 +1,31 @@
 import base64
+import hashlib
 
 from Crypto.Cipher import AES
 from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Util.strxor import strxor
 from Crypto import Random
 
 
 class DecryptionError(Exception):
     pass
+
+
+def base64url_decode(raw):
+    rem = len(raw) % 4
+    if rem > 0:
+        raw += b'=' * (4 - rem)
+    return base64.urlsafe_b64decode(raw)
+
+
+def base64url_encode(raw):
+    return base64.urlsafe_b64encode(raw).rstrip('=')
+
+
+def get_hashclass(name):
+    if name in ['md5', 'sha1', 'sha256', 'sha384', 'sha512']:
+        return getattr(hashlib, name)
+    return None
 
 
 def aes_base64_encrypt(key, data):
@@ -38,3 +57,62 @@ def aes_base64_decrypt(key, payload, raise_on_error=True):
     aes_key = PBKDF2(key, iv)
     aes = AES.new(aes_key, AES.MODE_CFB, iv)
     return aes.decrypt(crypted)
+
+
+def aes_base64url_deterministic_encrypt(key, data, salt, hash_name='sha256', count=1):
+    hashclass = get_hashclass(hash_name)
+    if hashclass is None:
+        raise ValueError('invalid hash_name')
+    iv = hashclass(salt).digest()
+    aes_key = PBKDF2(key, iv, count=count)
+    key_size = len(aes_key)
+    md5 = hashclass(data).digest()[:key_size]
+    aes = AES.new(aes_key, AES.MODE_CFB, strxor(iv[:key_size], md5)[:key_size])
+    crypted = aes.encrypt(data)
+    return '%d$aes-128-%s$%s$%s' % (count, hash_name,
+                                    base64url_encode(md5[:key_size]),
+                                    base64url_encode(crypted))
+
+
+def aes_base64url_deterministic_decrypt(key, raw, salt, raise_on_error=True):
+    try:
+        try:
+            splitted = raw.split('$')
+        except:
+            raise DecryptionError('invalid crypted value', raw)
+        if len(splitted) != 4:
+            raise DecryptionError('invalid encoding, not enough parts', raw)
+        try:
+            count = int(splitted[0])
+        except ValueError:
+            raise DecryptionError('invalid encryption, count is not a number')
+        if not splitted[1].startswith('aes-128-'):
+            raise DecryptionError('invalid algorithm', splitted[1])
+        hashclass = get_hashclass(splitted[1][len('aes-128-'):])
+        if hashclass is None:
+            raise DecryptionError('invalid hash name', splitted[1])
+        try:
+            md5 = base64url_decode(splitted[2])
+        except TypeError:
+            raise DecryptionError('incorrect base64url encoding of hash', splitted[1])
+            return None
+        try:
+            crypted = base64url_decode(splitted[3])
+        except TypeError:
+            if raise_on_error:
+                raise DecryptionError('incorrect base64url encoding of crypted', splitted[2])
+            return None
+        iv = hashclass(salt).digest()
+        aes_key = PBKDF2(key, iv, count=count)
+        key_size = len(aes_key)
+        if len(md5) != key_size:
+            raise DecryptionError('invalid hash length')
+        aes = AES.new(aes_key, AES.MODE_CFB, strxor(iv[:key_size], md5)[:len(aes_key)])
+        decrypted = aes.decrypt(crypted)
+        if hashclass(decrypted).digest()[:key_size] != md5:
+            raise DecryptionError('hash does not match decrypted value')
+        return decrypted
+    except DecryptionError:
+        if not raise_on_error:
+            return None
+        raise
--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -1,14 +1,18 @@
 import random
+import uuid
+import time
 
 import pytest
 from authentic2 import crypto
 
 key = '1234'
 
+
 def test_idempotency():
     for i in range(10):
         s = str(random.getrandbits(1024))
         assert crypto.aes_base64_decrypt(key, crypto.aes_base64_encrypt(key, s)) == s
+
 
 def test_exceptions():
     with pytest.raises(crypto.DecryptionError):
@@ -18,3 +22,28 @@ def test_exceptions():
     assert crypto.aes_base64_decrypt(key, 'xxxx', raise_on_error=False) is None
     assert crypto.aes_base64_decrypt(key, 'xxx$y', raise_on_error=False) is None
 
+
+def test_deterministic_encryption():
+    salt = '4567'
+    raw = uuid.uuid4().bytes
+
+    for hash_name in ['md5', 'sha1', 'sha256', 'sha384', 'sha512']:
+        for count in [0, 1, 50]:
+            crypted1 = crypto.aes_base64url_deterministic_encrypt(key, raw, salt,
+                                                                  hash_name=hash_name, count=count)
+            crypted2 = crypto.aes_base64url_deterministic_encrypt(key, raw, salt,
+                                                                  hash_name=hash_name, count=count)
+            assert crypted1 == crypted2
+            print 'Crypted', hash_name, count, len(crypted1), crypted1
+
+            t = time.time()
+            for i in range(100):
+                crypted1 = crypto.aes_base64url_deterministic_encrypt(key, raw, salt,
+                                                                      hash_name=hash_name,
+                                                                      count=count)
+            print 'Encryption time:', hash_name, count, (time.time() - t) / 100.0
+
+            t = time.time()
+            for i in range(1000):
+                assert crypto.aes_base64url_deterministic_decrypt(key, crypted1, salt) == raw
+            print 'Decryption time:', hash_name, count, (time.time() - t) / 1000.0
