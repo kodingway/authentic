6a04b5e89e2fb7c92f94ae14f35ed1af346b877c
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2017-07-28 09:33:02 +0200

    custom_user: convert migration 0011 to a post_migrate signal (fixes #17948)
    Using migrations for fixtures is bad.

--- src/authentic2/custom_user/apps.py
+++ src/authentic2/custom_user/apps.py
@@ -4,3 +4,57 @@ from django.apps import AppConfig
 class CustomUserConfig(AppConfig):
     name = 'authentic2.custom_user'
     verbose_name = 'Authentic2 Custom User App'
+
+    def ready(self):
+        from django.db.models.signals import post_migrate
+
+        post_migrate.connect(
+            self.create_first_name_last_name_attributes,
+            sender=self)
+
+    def create_first_name_last_name_attributes(self, app_config, **kwargs):
+        from django.utils import translation
+        from django.utils.translation import ugettext_lazy as _
+        from django.conf import settings
+        from authentic2.attribute_kinds import get_kind
+        from authentic2.models import Attribute, AttributeValue
+        from django.contrib.auth import get_user_model
+        from django.contrib.contenttypes.models import ContentType
+
+        if Attribute.objects.filter(name__in=['first_name', 'last_name']).count() == 2:
+            return
+
+        translation.activate(settings.LANGUAGE_CODE)
+        User = get_user_model()
+        content_type = ContentType.objects.get_for_model(User)
+
+        attrs = {}
+        attrs['first_name'], created = Attribute.objects.get_or_create(
+            name='first_name',
+            defaults={'kind': 'string',
+                      'label': _('First name'),
+                      'required': True,
+                      'asked_on_registration': True,
+                      'user_editable': True,
+                      'user_visible': True})
+        attrs['last_name'], created = Attribute.objects.get_or_create(
+            name='last_name',
+            defaults={'kind': 'string',
+                      'label': _('Last name'),
+                      'required': True,
+                      'asked_on_registration': True,
+                      'user_editable': True,
+                      'user_visible': True})
+
+        serialize = get_kind('string').get('serialize')
+        for user in User.objects.all():
+            for attr_name in attrs:
+                av, created = AttributeValue.objects.get_or_create(
+                    content_type=content_type,
+                    object_id=user.id,
+                    attribute=attrs[attr_name],
+                    defaults={
+                        'multiple': False,
+                        'verified': False,
+                        'content': serialize(getattr(user, attr_name, None))
+                    })
--- src/authentic2/custom_user/migrations/0011_manual_attribute_values_for_name_fields.py
+++ src/authentic2/custom_user/migrations/0011_manual_attribute_values_for_name_fields.py
@@ -1,66 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf import settings
 from django.db import migrations
-from django.utils import translation
-from django.utils.translation import ugettext_lazy as _
-
-from authentic2.attribute_kinds import get_kind
 
 
 def create_attribute_value_for_names(apps, schema_editor):
-    translation.activate(settings.LANGUAGE_CODE)
-
-    Attribute = apps.get_model('authentic2', 'Attribute')
-    AttributeValue = apps.get_model('authentic2', 'AttributeValue')
-    User = apps.get_model('custom_user', 'User')
-    ContentType = apps.get_model('contenttypes', 'ContentType')
-
-    # django.contrib.contenttypes.management.update_contenttypes cannot be used
-    # as we don't have a real app_config object.  Therefore we insert an entry
-    # in content type table if it didn't exist.
-    content_type, created = ContentType.objects.get_or_create(
-            model='user', app_label='custom_user')
-    if created:
-        content_type.save()
-
-    attrs = {}
-    attrs['first_name'], created = Attribute.objects.get_or_create(
-        name='first_name',
-        defaults={'kind': 'string',
-                  'label': _('First name'),
-                  'required': True,
-                  'asked_on_registration': True,
-                  'user_editable': True,
-                  'user_visible': True})
-    if created:
-        attrs['first_name'].save()
-
-    attrs['last_name'], created = Attribute.objects.get_or_create(
-        name='last_name',
-        defaults={'kind': 'string',
-                  'label': _('Last name'),
-                  'required': True,
-                  'asked_on_registration': True,
-                  'user_editable': True,
-                  'user_visible': True})
-
-    if created:
-        attrs['last_name'].save()
-
-    serialize = get_kind('string').get('serialize')
-    for user in User.objects.all():
-        for attr_name in ('first_name', 'last_name'):
-            av, created = AttributeValue.objects.get_or_create(
-                content_type=content_type,
-                object_id=user.id,
-                attribute=attrs[attr_name],
-                multiple=False,
-                verified=False,
-                content=serialize(getattr(user, attr_name, None)))
-            if created:
-                av.save()
+    pass
 
 
 class Migration(migrations.Migration):
