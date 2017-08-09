5c684f6837b2c1662c7a0b869f93235077a4e16e
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2017-08-09 12:07:51 +0200

    js: remove need for jQuery in js_seconds_until.js (fixes #17944)
    It's rewritten using pure DOM API.

--- src/authentic2/static/authentic2/js/js_seconds_until.js
+++ src/authentic2/static/authentic2/js/js_seconds_until.js
@@ -1,34 +1,23 @@
-
-
-if ($('.js-seconds-until').length) {
-  var toto = function() {
-    var timeout_id;
-    var initial_time = Date.now();
-    var $spans = $('.js-seconds-until');
-    for (var i = 0; i < $spans.length; i++) {
-      var $span = $($spans[i]);
-      $span.data('until', initial_time + parseInt($span.text())*1000);
-    }
-
-    function decrease_seconds() {
-      var $spans = $('.js-seconds-until');
-      if (! $spans.length) {
-        window.clearInterval(timeout_id);
-      } else {
-        var now = Date.now();
-        for (var i = 0; i < $spans.length; i++) {
-          var $span = $($spans[i]);
-          var until = $span.data('until');
-          var duration = (until - now) / 1000;
-          if (duration < 1) {
-            $span.parent().remove();
-          } else {
-            $span.text(Math.floor(duration));
-          }
-        }
-      }
-    }
-    setInterval(decrease_seconds, 500);
+(function () {
+  var spans = document.getElementsByClassName('js-seconds-until');
+  if (! spans.length) {
+    return;
   }
-  toto();
-}
+  var span = spans[0];
+  var timeout_id;
+  var initial_time = Date.now();
+  var until = initial_time + parseInt(span.textContent) * 1000;
+
+  function decrease_seconds() {
+    var now = Date.now();
+    var duration = (until - now) / 1000;
+    if (duration < 1) {
+       /* remove the container */
+       span.parentNode.parentNoderemoveChild(span.parentNode);
+    } else {
+       /* decrease seconds before retry */
+       span.textContent = Math.floor(duration).toString();
+    }
+  }
+  timeout_id = setInterval(decrease_seconds, 500);
+})()
